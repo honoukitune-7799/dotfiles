7fbfb90d7718ef9e68863e38f714d7fff52c0abd
Nobuchika Tanaka <honoukitune.7799@gmail.com>
2018-10-04 18:59:26 +0900

    Add vim, nvim config files

--- install/common/make_links.sh
+++ install/common/make_links.sh
@@ -2,3 +2,11 @@
 
 # Etc
 ln -sfn ~/dotfiles/config ~/.config
+
+# Vim
+ln -sfn ~/dotfiles/vimrc ~/.vimrc
+ln -sfn ~/dotfiles/vim ~/.vim
+
+# Neovim
+ln -sfn ~/dotfiles/vim ~/.config/nvim
+ln -sfn ~/dotfiles/vimrc ~/.config/nvim/init.vim
